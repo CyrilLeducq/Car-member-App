4e83bed4649bebb5f5a57743835ddc07801a29d5
Rreems <walen212@gmail.com>
2025-03-28 14:56:06 +0100

    Creation dossiers organisation du projet

--- CarMemberApp.Server/obj/Debug/net8.0/CarMemberApp.Server.AssemblyInfo.cs
+++ CarMemberApp.Server/obj/Debug/net8.0/CarMemberApp.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarMemberApp.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+473456bd23c190ca1f34f4aa93c85a45fa92bba3")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarMemberApp.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarMemberApp.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
